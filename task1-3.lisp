b1a16e48a6ff51d55cecf9cd2cbf64475ee6ebde
Anton <popkov99@yandex.ru>
2019-12-14 11:06:26 +0300

    modified task1-3

--- task1-3.lisp
+++ task1-3.lisp
@@ -1,6 +1,7 @@
 (defun index (e lst)
-      (cond ((eq lst nil) "Элемент не найден!" )
-        ((eq e (car lst)) 0)
-        (t (+ 1 (index e (cdr lst))))))
+      (if (null lst) 
+      (if nil (reverse nil) 0)
+      (if (equal e (car list))
+        (t (+ 1 (index e (cdr lst)))))))
 
 (index '2 '(12 13 14 2 16 18))
