e2dfa9fe069ba51ef9bfcd68857e52eddfe9de76
Anton <popkov99@yandex.ru>
2019-12-14 10:10:21 +0300

    task1-3 added

--- task1-3.lisp
+++ task1-3.lisp
@@ -1,4 +1,6 @@
-(defun index (e x)
-  (cond ((eq e (car x)) 0)
-        (t (+ 1 (index e (cdr x))))))
+(defun index (e lst)
+      (cond ((eq lst nil) "НЕ НАЙДЕН!" )
+        ((eq e (car lst)) 0)
+        (t (+ 1 (index e (cdr lst))))))
+
 (index '2 '(12 13 14 2 16 18))
